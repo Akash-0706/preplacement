b8e2bfb9216496ba001f55093c694f0881f6fc7e
Akash <93485744+Akash-0706@users.noreply.github.com>
2023-10-04 20:34:06 +0530

    Update permutationUsingLoop.cpp

--- permutationUsingLoop.cpp
+++ permutationUsingLoop.cpp
@@ -30,6 +30,7 @@ int main() {
             }
         }
     }
+    
 
     for (const std::string& perm : permutations) {
         std::cout << perm << std::endl;
@@ -71,3 +72,35 @@ int main() {
 
     return 0;
 }
+
+// repetition not  allowed
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    std::string input_str = "ABCD";
+    int n = input_str.length();
+
+    std::vector<std::string> permutations;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+             for (int k = 0; k < n; ++k) {
+                
+                    std::string permutation = "";
+                    permutation += input_str[i];
+                    permutation += input_str[j];
+                    permutation += input_str[k];
+                    permutation += input_str[6-i-j-k];
+                    permutations.push_back(permutation);
+                 
+            }
+        }
+    }
+
+    for (const std::string& perm : permutations) {
+        std::cout << perm << std::endl;
+    }
+
+    
